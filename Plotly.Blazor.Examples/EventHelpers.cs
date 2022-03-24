246a5327d2c1a1c3e9a1ac7a4ef6f56e007582e2
Jacek.Papiniak <jacek.papiniak@atmosi.com>
2022-03-24 09:59:24 +0000

    Emit click event that returns chart type and pie chart

---
+++ Plotly.Blazor.Examples/EventHelpers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Plotly.Blazor.Traces;
+
+namespace Plotly.Blazor.Examples
+{
+
+    public class XYClickEventArgs : EventArgs
+    {
+        public string X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class PieClickEventArgs : EventArgs
+    {
+        public string Label { get; set; }
+        public int Value { get; set; }
+    }
+    
+    public class PieHoverEventArgs : EventArgs
+    {
+        public string Label { get; set; }
+        public int Value { get; set; }
+    }
+    
+    public class PlotlyClickEvent : EventArgs
+    {
+        public Pie Value { get; set; }
+        public string ChartType { get; set; }
+    }
+    
+    public class NoDataEventArgs : EventArgs { }
+
+    [EventHandler("onplotly_click_xy", typeof(XYClickEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    [EventHandler("onplotly_click_pie", typeof(PieClickEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    [EventHandler("onplotly_hover_pie", typeof(PieHoverEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    [EventHandler("onplotly_doubleclick", typeof(NoDataEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    
+    [EventHandler("onplotly_click", typeof(PlotlyClickEvent), enableStopPropagation: true, enablePreventDefault: true)]
+    public static class EventHandlers { }
+}
---
+++ Plotly.Blazor.Examples/wwwroot/plotly-events.js
@@ -0,0 +1,49 @@
+﻿window.plotlyExampleEvents = {
+    plotlyClickXYEventArgsCreator: function (event) {
+        return {
+            x: event.detail.points[0].x,
+            y: event.detail.points[0].y
+        };
+    },
+    plotlyPIEEventArgsCreator: function (event) {
+        return {
+            label: event.detail.points[0].label,
+            value: event.detail.points[0].value
+        };
+    },
+    plotlyNoDataEventArgsCreator: function (event) {
+        // No data associated with event
+        return { };
+    },
+    plotlyClickEventArgsCreator: function (event) {
+        console.log(event);
+        return {
+            value: event.detail.points[0].data,
+            chartType : event.detail.points[0].fullData.type
+        }
+    },
+}
+
+// Register custom event names and handlers
+Blazor.registerCustomEventType('plotly_click_pie', {
+    browserEventName: 'plotly_click',
+    createEventArgs: plotlyExampleEvents.plotlyPIEEventArgsCreator
+});
+Blazor.registerCustomEventType('plotly_hover_pie', {
+    browserEventName: 'plotly_hover',
+    createEventArgs: plotlyExampleEvents.plotlyPIEEventArgsCreator
+});
+Blazor.registerCustomEventType('plotly_click_xy', {
+    browserEventName: 'plotly_click',
+    createEventArgs: plotlyExampleEvents.plotlyClickXYEventArgsCreator
+});
+Blazor.registerCustomEventType('plotly_doubleclick', {
+    browserEventName: 'plotly_doubleclick',
+    createEventArgs: plotlyExampleEvents.plotlyNoDataEventArgsCreator
+});
+
+
+Blazor.registerCustomEventType('plotly_click', {
+    browserEventName: 'plotly_click',
+    createEventArgs: plotlyExampleEvents.plotlyClickEventArgsCreator
+});
